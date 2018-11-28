18e39f9892651fedb5a2cc2bd4b5b50930812e81
arturgontijo <arturgontijo@gmail.com>
2018-11-28 13:49:58 -0200

    [Repo] CNTK tutorial

--- test.py
+++ test.py
@@ -60,24 +60,24 @@ def is_test(): return envvar in os.environ
 
 def download(file_name):
     try:
-        data = get_stock_data("SPY", 2015, 1, 2, 2017, 1, 1)
+        ts_data = get_stock_data("SPY", 2010, 1, 2, 2017, 1, 1)
     except:
         raise Exception("Data could not be downloaded")
 
-    dir = os.path.dirname(file_name)
+    dir_name = os.path.dirname(file_name)
 
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
 
     if not os.path.isfile(file_name):
         print("Saving", file_name)
         with open(file_name, 'wb') as f:
-            pkl.dump(data, f, protocol=2)
-    return data
+            pkl.dump(ts_data, f, protocol=2)
+    return ts_data
 
 
-def create_model(input, num_output_classes, num_hidden_layers, hidden_layers_dim):
-    h = input
+def create_model(net_input, num_output_classes, num_hidden_layers, hidden_layers_dim):
+    h = net_input
     with C.layers.default_options(init=C.glorot_uniform()):
         for i in range(num_hidden_layers):
             h = C.layers.Dense(hidden_layers_dim,
@@ -134,56 +134,56 @@ def main():
         # Check for data in local cache
         if os.path.exists(data_file):
             print("File already exists", data_file)
-            data = pd.read_pickle(data_file)
+            ts_data = pd.read_pickle(data_file)
         else:
             # If not there we might be running in CNTK's test infrastructure
             if is_test():
                 test_file = os.path.join(os.environ[envvar], 'Tutorials', 'data', 'stock', 'stock_SPY.pkl')
                 if os.path.isfile(test_file):
                     print("Reading data from test data directory")
-                    data = pd.read_pickle(test_file)
+                    ts_data = pd.read_pickle(test_file)
                 else:
                     print("Test data directory missing file", test_file)
                     print("Downloading data from Google Finance")
-                    data = download(data_file)
+                    ts_data = download(data_file)
             else:
                 # Local cache is not present and not test env
                 # download the data from Google finance and cache it in a local directory
                 # Please check if there is trade data for the chosen stock symbol during this period
-                data = download(data_file)
+                ts_data = download(data_file)
 
         # Feature name list
         predictor_names = []
 
         # Compute price difference as a feature
-        data["diff"] = np.abs((data["Close"] - data["Close"].shift(1)) / data["Close"]).fillna(0)
+        ts_data["diff"] = np.abs((ts_data["Close"] - ts_data["Close"].shift(1)) / ts_data["Close"]).fillna(0)
         predictor_names.append("diff")
 
         # Compute the volume difference as a feature
-        data["v_diff"] = np.abs((data["Volume"] - data["Volume"].shift(1)) / data["Volume"]).fillna(0)
+        ts_data["v_diff"] = np.abs((ts_data["Volume"] - ts_data["Volume"].shift(1)) / ts_data["Volume"]).fillna(0)
         predictor_names.append("v_diff")
 
         # Compute the stock being up (1) or down (0) over different day offsets compared to current dat closing price
         num_days_back = 8
 
         for i in range(1, num_days_back + 1):
-            data["p_" + str(i)] = np.where(data["Close"] > data["Close"].shift(i), 1, 0)  # i: number of look back days
+            data["p_" + str(i)] = np.where(ts_data["Close"] > ts_data["Close"].shift(i), 1, 0)  # i: number of look back days
             predictor_names.append("p_" + str(i))
 
         # If you want to save the file to your local drive
         # data.to_csv("PATH_TO_SAVE.csv")
-        data.head(10)
+        ts_data.head(10)
 
-        data["next_day"] = np.where(data["Close"].shift(-1) > data["Close"], 1, 0)
-        data["next_day_opposite"] = np.where(data["next_day"] == 1, 0, 1)  # The label must be one-hot encoded
+        ts_data["next_day"] = np.where(ts_data["Close"].shift(-1) > ts_data["Close"], 1, 0)
+        ts_data["next_day_opposite"] = np.where(ts_data["next_day"] == 1, 0, 1)  # The label must be one-hot encoded
 
         # Establish the start and end date of our training timeseries (picked 2000 days before the market crash)
-        training_data = data["2001-02-05":"2009-01-20"]
+        training_data = ts_data["2001-02-05":"2009-01-20"]
 
         # We define our test data as: data["2008-01-02":]
         # This example allows to include data up to current date
 
-        test_data = data["2009-01-20":"2016-12-29"]
+        test_data = ts_data["2009-01-20":"2016-12-29"]
         training_features = np.asarray(training_data[predictor_names], dtype="float32")
         training_labels = np.asarray(training_data[["next_day", "next_day_opposite"]], dtype="float32")
 
@@ -229,7 +229,7 @@ def main():
             labels = np.ascontiguousarray(tl[i % num_minibatches])
 
             # Specify the mapping of input variables in the model to actual minibatch data to be trained with
-            trainer.train_minibatch({input: features, label: labels})
+            trainer.train_minibatch({net_input: features, label: labels})
             batchsize, loss, error = print_training_progress(trainer, i, training_progress_output_freq, verbose=1)
             if not (loss == "NA" or error == "NA"):
                 plotdata["batchsize"].append(batchsize)
@@ -242,11 +242,11 @@ def main():
         test_features = np.ascontiguousarray(test_data[predictor_names], dtype="float32")
         test_labels = np.ascontiguousarray(test_data[["next_day", "next_day_opposite"]], dtype="float32")
 
-        avg_error = trainer.test_minibatch({input: test_features, label: test_labels})
+        avg_error = trainer.test_minibatch({net_input: test_features, label: test_labels})
         print("Average error: {0:2.2f}%".format(avg_error * 100))
 
         out = C.softmax(z)
-        predicted_label_prob = out.eval({input: test_features})
+        predicted_label_prob = out.eval({net_input: test_features})
         test_data["p_up"] = pd.Series(predicted_label_prob[:, 0], index=test_data.index)
         test_data["p_down"] = predicted_label_prob[:, 1]
         test_data['long_entries'] = np.where((test_data.p_up > 0.55), 1, 0)
