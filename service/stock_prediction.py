a6c67b25d9a617f22c0b980efb2cfd27d793f909
arturgontijo <arturgontijo@gmail.com>
2018-11-29 10:02:53 -0200

    [Repo] Service Init

--- service/stock_prediction.py
+++ service/stock_prediction.py
@@ -69,121 +69,134 @@ class StockPrediction:
         return []
 
     def stock_prediction(self):
-        stock_data = self._get_stock_data()
-        print(stock_data)
-        # Feature name list
-        predictor_names = []
+        try:
+            stock_data = self._get_stock_data()
+            print(stock_data)
+            # Feature name list
+            predictor_names = []
 
-        if "Close" in stock_data and "Volume" in stock_data:
-            close_tag = "Close"
-            volume_tag = "Volume"
-        elif "close" in stock_data and "volume" in stock_data:
-            close_tag = "close"
-            volume_tag = "volume"
-        else:
+            if "Close" in stock_data and "Volume" in stock_data:
+                close_tag = "Close"
+                volume_tag = "Volume"
+            elif "close" in stock_data and "volume" in stock_data:
+                close_tag = "close"
+                volume_tag = "volume"
+            else:
+                return {
+                    "Error": "Couldn't find Close|Volume data"
+                }
+
+            # Compute price difference as a feature
+            stock_data["diff"] = np.abs(
+                (stock_data[close_tag] - stock_data[close_tag].shift(1)) / stock_data[close_tag]).fillna(0)
+            predictor_names.append("diff")
+
+            # Compute the volume difference as a feature
+            stock_data["v_diff"] = np.abs(
+                (stock_data[volume_tag] - stock_data[volume_tag].shift(1)) / stock_data[volume_tag]).fillna(0)
+            predictor_names.append("v_diff")
+
+            # Compute the stock being up (1) or down (0) over different day offsets compared to current closing price
+            num_days_back = 8
+            for i in range(1, num_days_back + 1):
+                # i: number of look back days
+                stock_data["p_" + str(i)] = np.where(stock_data[close_tag] > stock_data[close_tag].shift(i), 1, 0)
+                predictor_names.append("p_" + str(i))
+
+            stock_data["next_day"] = np.where(stock_data[close_tag].shift(-1) > stock_data[close_tag], 1, 0)
+
+            # The label must be one-hot encoded
+            stock_data["next_day_opposite"] = np.where(stock_data["next_day"] == 1, 0, 1)
+
+            # Establish the start and end date of our training timeseries
+            training_data = stock_data[self.start:self.end]
+
+            training_features = np.asarray(training_data[predictor_names], dtype="float32")
+            training_labels = np.asarray(training_data[["next_day", "next_day_opposite"]], dtype="float32")
+
+            # Lets build the network
+            input_dim = 2 + num_days_back
+            # Remember we need to have 2 since we are trying to classify if the market goes up or down 1 hot encoded
+            num_output_classes = 2
+            num_hidden_layers = 2
+            hidden_layers_dim = 2 + num_days_back
+            input_dynamic_axes = [C.Axis.default_batch_axis()]
+            net_input = C.input_variable(input_dim, dynamic_axes=input_dynamic_axes)
+            label = C.input_variable(num_output_classes, dynamic_axes=input_dynamic_axes)
+
+            z = self._create_model(net_input, num_output_classes, num_hidden_layers, hidden_layers_dim)
+            loss = C.cross_entropy_with_softmax(z, label)
+            label_error = C.classification_error(z, label)
+            lr_per_minibatch = C.learning_parameter_schedule(0.125)
+            trainer = C.Trainer(z, (loss, label_error), [C.sgd(z.parameters, lr=lr_per_minibatch)])
+
+            # Initialize the parameters for the trainer, we will train in large minibatches in sequential order
+            minibatch_size = 100
+            num_minibatches = len(training_data.index) // minibatch_size
+
+            # Run the trainer on and perform model training
+            training_progress_output_freq = 1
+
+            # Visualize the loss over minibatch
+            plotdata = {"batchsize": [], "loss": [], "error": []}
+
+            # It is key that we make only one pass through the data linearly in time
+            num_passes = 1
+
+            print("len(training_features) 1: ", len(training_features))
+            print("len(training_labels) 1: ", len(training_labels))
+
+            l_training_features = len(training_features)
+            training_features = training_features[:l_training_features - (l_training_features % num_minibatches)]
+            l_training_labels = len(training_labels)
+            training_labels = training_labels[:l_training_labels - (l_training_labels % num_minibatches)]
+
+            print("len(training_features) 2: ", len(training_features))
+            print("len(training_labels) 2: ", len(training_labels))
+
+            # Train our neural network
+            tf = np.split(training_features, num_minibatches)
+            tl = np.split(training_labels, num_minibatches)
+
+            for i in range(num_minibatches * num_passes):  # multiply by the
+                features = np.ascontiguousarray(tf[i % num_minibatches])
+                labels = np.ascontiguousarray(tl[i % num_minibatches])
+
+                # Specify the mapping of input variables in the model to actual minibatch data to be trained with
+                trainer.train_minibatch({net_input: features, label: labels})
+                batchsize, loss, error = self._print_training_progress(trainer, i, training_progress_output_freq, verbose=1)
+                if not (loss == "NA" or error == "NA"):
+                    plotdata["batchsize"].append(batchsize)
+                    plotdata["loss"].append(loss)
+                    plotdata["error"].append(error)
+
+            # Now that we have trained the net, and we will do out of sample test to see how we did.
+            # and then more importantly analyze how that set did
+
+            test_data = stock_data[self.target_date:self.target_date]
+
+            print("=================TEST DATA================")
+            print(test_data)
+            print("==========================================")
+
+            test_features = np.ascontiguousarray(test_data[predictor_names], dtype="float32")
+            test_labels = np.ascontiguousarray(test_data[["next_day", "next_day_opposite"]], dtype="float32")
+
+            avg_error = trainer.test_minibatch({net_input: test_features, label: test_labels})
+            print("Average error: {0:2.2f}%".format(avg_error * 100))
+
+            sm_out = C.softmax(z)
+            predicted_label_prob = sm_out.eval({net_input: test_features})
+            test_data["p_up"] = pd.Series(predicted_label_prob[:, 0], index=test_data.index)
+            test_data["p_down"] = predicted_label_prob[:, 1]
+
             return {
-                "Error": "Couldn't find Close|Volume data"
+                "p_up": test_data["p_up"],
+                "p_down": test_data["p_down"]
             }
 
-        # Compute price difference as a feature
-        stock_data["diff"] = np.abs(
-            (stock_data[close_tag] - stock_data[close_tag].shift(1)) / stock_data[close_tag]).fillna(0)
-        predictor_names.append("diff")
-
-        # Compute the volume difference as a feature
-        stock_data["v_diff"] = np.abs(
-            (stock_data[volume_tag] - stock_data[volume_tag].shift(1)) / stock_data[volume_tag]).fillna(0)
-        predictor_names.append("v_diff")
-
-        # Compute the stock being up (1) or down (0) over different day offsets compared to current closing price
-        num_days_back = 8
-        for i in range(1, num_days_back + 1):
-            # i: number of look back days
-            stock_data["p_" + str(i)] = np.where(stock_data[close_tag] > stock_data[close_tag].shift(i), 1, 0)
-            predictor_names.append("p_" + str(i))
-
-        stock_data["next_day"] = np.where(stock_data[close_tag].shift(-1) > stock_data[close_tag], 1, 0)
-
-        # The label must be one-hot encoded
-        stock_data["next_day_opposite"] = np.where(stock_data["next_day"] == 1, 0, 1)
-
-        # Establish the start and end date of our training timeseries
-        training_data = stock_data[self.start:self.end]
-
-        training_features = np.asarray(training_data[predictor_names], dtype="float32")
-        training_labels = np.asarray(training_data[["next_day", "next_day_opposite"]], dtype="float32")
-
-        # Lets build the network
-        input_dim = 2 + num_days_back
-        # Remember we need to have 2 since we are trying to classify if the market goes up or down 1 hot encoded
-        num_output_classes = 2
-        num_hidden_layers = 2
-        hidden_layers_dim = 2 + num_days_back
-        input_dynamic_axes = [C.Axis.default_batch_axis()]
-        net_input = C.input_variable(input_dim, dynamic_axes=input_dynamic_axes)
-        label = C.input_variable(num_output_classes, dynamic_axes=input_dynamic_axes)
-
-        z = self._create_model(net_input, num_output_classes, num_hidden_layers, hidden_layers_dim)
-        loss = C.cross_entropy_with_softmax(z, label)
-        label_error = C.classification_error(z, label)
-        lr_per_minibatch = C.learning_parameter_schedule(0.125)
-        trainer = C.Trainer(z, (loss, label_error), [C.sgd(z.parameters, lr=lr_per_minibatch)])
-
-        # Initialize the parameters for the trainer, we will train in large minibatches in sequential order
-        minibatch_size = 100
-        num_minibatches = len(training_data.index) // minibatch_size
-
-        # Run the trainer on and perform model training
-        training_progress_output_freq = 1
-
-        # Visualize the loss over minibatch
-        plotdata = {"batchsize": [], "loss": [], "error": []}
-
-        # It is key that we make only one pass through the data linearly in time
-        num_passes = 1
-
-        l_training_features = len(training_features)
-        training_features = training_features[:l_training_features - (l_training_features % num_minibatches)]
-        l_training_labels = len(training_labels)
-        training_labels = training_labels[:l_training_labels - (l_training_labels % num_minibatches)]
-
-        # Train our neural network
-        tf = np.split(training_features, num_minibatches)
-        tl = np.split(training_labels, num_minibatches)
-
-        for i in range(num_minibatches * num_passes):  # multiply by the
-            features = np.ascontiguousarray(tf[i % num_minibatches])
-            labels = np.ascontiguousarray(tl[i % num_minibatches])
-
-            # Specify the mapping of input variables in the model to actual minibatch data to be trained with
-            trainer.train_minibatch({net_input: features, label: labels})
-            batchsize, loss, error = self._print_training_progress(trainer, i, training_progress_output_freq, verbose=1)
-            if not (loss == "NA" or error == "NA"):
-                plotdata["batchsize"].append(batchsize)
-                plotdata["loss"].append(loss)
-                plotdata["error"].append(error)
-
-        # Now that we have trained the net, and we will do out of sample test to see how we did.
-        # and then more importantly analyze how that set did
-
-        test_data = stock_data[self.target_date:self.target_date]
-
-        test_features = np.ascontiguousarray(test_data[predictor_names], dtype="float32")
-        test_labels = np.ascontiguousarray(test_data[["next_day", "next_day_opposite"]], dtype="float32")
-
-        avg_error = trainer.test_minibatch({net_input: test_features, label: test_labels})
-        print("Average error: {0:2.2f}%".format(avg_error * 100))
-
-        out = C.softmax(z)
-        predicted_label_prob = out.eval({net_input: test_features})
-        test_data["p_up"] = pd.Series(predicted_label_prob[:, 0], index=test_data.index)
-        test_data["p_down"] = predicted_label_prob[:, 1]
-        test_data['positions'] = test_data["long_entries"].fillna(0) + test_data["short_entries"].fillna(0)
-        test_data["pnl"] = test_data[close_tag].diff().shift(-1).fillna(0) * test_data["positions"] / np.where(
-            test_data[close_tag] != 0, test_data[close_tag], 1)
-        test_data["perc"] = (test_data[close_tag] - test_data[close_tag].shift(1)) / test_data[close_tag].shift(1)
-
-        return {
-            "p_up": test_data["p_up"],
-            "p_down": test_data["p_down"]
-        }
+        except Exception as e:
+            traceback.print_exc()
+            return {
+                "Error": str(e)
+            }
