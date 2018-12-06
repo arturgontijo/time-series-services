0e7a866164559d8c85edd6193f0f58f289939e05
arturgontijo <arturgontijo@gmail.com>
2018-12-06 12:37:07 -0200

    [TimeSeriesSAX] Initial Test

--- sax_forecast_v2.py
+++ sax_forecast_v2.py
@@ -181,7 +181,7 @@ def main():
         # a.plot((result_y[ds]).flatten(), label=ds + " raw")
         # a.plot(np.array(results), label=ds + " pred")
 
-        last_p_result = []
+        last_p_y = []
         for idx, i in enumerate(result_y[ds]):
             if idx % (word_len - 2) == 0:
                 norm_i = -1
@@ -191,9 +191,9 @@ def main():
                         break
                     elif i >= 1:
                         norm_i = v[1]
-                last_p_result.append(norm_i)
+                    last_p_y.append(norm_i)
 
-        a.plot(np.array(last_p_result).flatten(), label=ds + " raw")
+        a.plot(np.array(last_p_y).flatten(), label=ds + " raw")
 
         last_p_result = []
         for idx, i in enumerate(results):
@@ -207,10 +207,17 @@ def main():
                         norm_i = v[1]
                 last_p_result.append(norm_i)
 
-        a.plot(np.array(last_p_result), label=ds + " pred")
+        a.plot(np.array(last_p_result).flatten(), label=ds + " pred")
         a.legend()
 
         fig.savefig("{}_chart_{}_epochs.jpg".format(ds, epochs))
+
+        correct_pred = 0
+        for idx, i in enumerate(last_p_y):
+            if last_p_result[idx] == last_p_y[idx]:
+                correct_pred += 1
+
+        print("Set({}): {}/{} = {:.2f}".format(ds, correct_pred, len(last_p_y), float(correct_pred / len(last_p_y))))
 
     print("Delta: ", time.time() - start_time)
 
