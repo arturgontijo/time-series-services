44fad37cb805e39510de00f661798e4f79a356c4
arturgontijo <arturgontijo@gmail.com>
2018-12-11 11:54:43 -0200

    [TimeSeriesSAX] Stocks - Tests

--- sax_forecast_v2.py
+++ sax_forecast_v2.py
@@ -138,9 +138,6 @@ def prepare_data(window_len, word_len, alphabet_len, alpha_to_num):
             for i in v:
                 my_sax[i] = k
 
-        for i in my_sax:
-            print("my_sax[{}]: {}".format(i, my_sax[i]))
-
         tmp_d = {"x": [], "y": []}
         for i in range(len(my_sax)):
             word = my_sax[i]
